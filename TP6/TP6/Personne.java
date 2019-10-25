0a5cc3c867536ff47e6f7bb55462dd71313f5028
GauthB <gauthier.bohyn@gmail.com>
2019-10-25 10:49:43 +0200

    update

--- TP6/TP6/Personne.java
+++ TP6/TP6/Personne.java
@@ -10,5 +10,52 @@ package TP6;
 public class Personne {
 
 	
+	protected String nom;
+	protected String prenom;
+	protected int numRegistreNationnal;
+	/**
+	 * @return the nom
+	 */
+	public String getNom() {
+		return nom;
+	}
+	/**
+	 * @param nom the nom to set
+	 */
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	/**
+	 * @return the prenom
+	 */
+	public String getPrenom() {
+		return prenom;
+	}
+	/**
+	 * @param prenom the prenom to set
+	 */
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	/**
+	 * @return the numRegistreNationnal
+	 */
+	public int getNumRegistreNationnal() {
+		return numRegistreNationnal;
+	}
+	/**
+	 * @param numRegistreNationnal the numRegistreNationnal to set
+	 */
+	public void setNumRegistreNationnal(int numRegistreNationnal) {
+		this.numRegistreNationnal = numRegistreNationnal;
+	}
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Personne [nom=" + nom + ", prenom=" + prenom + ", numRegistreNationnal=" + numRegistreNationnal + "]";
+	}
+	
 	
 }
